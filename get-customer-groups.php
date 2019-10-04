5e046282f928124bd0d25d598cbd0b6b961b7109
Robert <mrthinlt@gmail.com>
2019-10-04 14:39:45 +0700

    Create get-customer-groups.php

---
+++ get-customer-groups.php
@@ -0,0 +1,30 @@
+/**
+ * Customer Group
+ *
+ * @var \Magento\Customer\Model\ResourceModel\Group\Collection
+ */
+protected $_customerGroup;
+    
+/**
+ * @param \Magento\Backend\Block\Template\Context $context
+ * @param \Magento\Customer\Model\ResourceModel\Group\Collection $customerGroup
+ * @param array $data
+ */
+public function __construct(
+    \Magento\Backend\Block\Template\Context $context,
+    \Magento\Customer\Model\ResourceModel\Group\Collection $customerGroup,        
+    array $data = []
+) {
+    $this->_customerGroup = $customerGroup;        
+    parent::__construct($context, $data);
+}
+/**
+ * Get customer groups
+ * 
+ * @return array
+ */ 
+public function getCustomerGroups() {
+    $customerGroups = $this->_customerGroup->toOptionArray();
+    array_unshift($customerGroups, array('value'=>'', 'label'=>'Any'));
+    return $customerGroups;
+}
